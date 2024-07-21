ae7733339a5a4b7d0253ff9219b99e49676923c5
pyoya1123 <pyoya1123@gmail.com>
2024-07-21 22:46:08 +0900

    14728 벼락치기

--- h.cpp
+++ h.cpp
@@ -6,34 +6,37 @@ using namespace std;
 #include <queue>
 #include <string.h>
 
-int N;
-vector<int> vec;
-int dp[1005];
+int N, T; // 단원 개수, 공부할 수 있는 총 시간
+vector<pair<int, int>> vec;
+int dp[10005][105];
+int ans = 0;
 
 int main()
 {
-    cin >> N;
+    cin >> N >> T;
     vec.resize(N + 1);
-    for (int i = 0; i < N; i++)
-        cin >> vec[i];
-
-    fill(&dp[0], &dp[0] + 1005, 100000);
-
-    dp[0] = 0;
-
-    for (int i = 1; i < N; i++)
+    for (int i = 1; i <= N; i++)
     {
-        for (int j = 0; j < i; j++)
+        cin >> vec[i].first >> vec[i].second; // 예상 공부 시간, 배점
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        pair<int, int> e = vec[i];
+        for (int w = 1; w <= T; w++)
         {
-            if (i <= j + vec[j])
+            if (e.first <= w)
             {
-                dp[i] = min(dp[i], dp[j] + 1);
+                dp[w][i] = max(dp[w][i - 1], dp[w - e.first][i - 1] + e.second);
             }
+            else
+            {
+                dp[w][i] = max(dp[w][i], dp[w][i - 1]);
+            }
+            if (w == T)
+                ans = max(ans, dp[w][i]);
         }
     }
 
-    if (dp[N - 1] == 100000)
-        cout << -1 << "\n";
-    else
-        cout << dp[N - 1] << "\n";
+    cout << ans << "\n";
 }
